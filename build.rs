3da2e3cda9eb804cc4d22da2031051a82376394e
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-05-02 17:25:45 +0900

    linux

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        // println!("cargo:rustc-link-search=native=C:\\OpenBLAS\\OpenBLAS-0.3.26-x64\\bin");
-        println!("cargo:rustc-link-lib=libopenblas");
-    }
+    //#[cfg(target_os = "windows")]
+    //{
+    //    // println!("cargo:rustc-link-search=native=C:\\OpenBLAS\\OpenBLAS-0.3.26-x64\\bin");
+    //    println!("cargo:rustc-link-lib=libopenblas");
+    //}
 
     #[cfg(target_os = "linux")]
     {
