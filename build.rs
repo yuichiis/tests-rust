5cba357ccaefc6a0c66a07adbe9e3d0ea2ef4527
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-05-02 17:22:39 +0900

    linux

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
 
     #[cfg(target_os = "linux")]
     {
-        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
+        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
         println!("cargo:rustc-link-lib=dylib=libopenblas");
     }
 }
